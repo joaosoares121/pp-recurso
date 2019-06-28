5ecc8fd6529ba0042e271b2edd25ed7a9320ec0b
Tiago Pinto <8150277@estg.ipp.pt>
2019-06-28 17:07:22 +0100

    TestJson Pessoa

---
+++ pp-recurso-2019/src/pp/recurso/pkg2019/Pessoa.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp.recurso.pkg2019;
+
+/**
+ *
+ * @author Tiago Pinto
+ */
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    
+    
+    
+}
---
+++ pp-recurso-2019/src/pp/recurso/pkg2019/TestJson.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pp.recurso.pkg2019;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonStreamParser;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Tiago Pinto
+ */
+public class TestJson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pessoa pessoas[] = new Pessoa[2];
+        String path = "C:\\Users\\Tiago Pinto\\Documents\\NetBeansProjects\\Trabalho_PP_RECURSO_2019\\pp-recurso\\pp-recurso-2019\\src\\pp\\recurso\\pkg2019\\exemplo.json";
+        BufferedReader reader = null;
+        
+        try {
+            reader = new BufferedReader(new FileReader(path));
+            
+            JsonStreamParser p = new JsonStreamParser(reader);
+            
+            JsonArray arr = (JsonArray) p.next();
+            int arr_size = arr.size();
+            
+            for (int i = 0; i < arr_size; i++) {
+                JsonElement arrayElement = arr.get(i);
+                JsonObject obj = arrayElement.getAsJsonObject();
+                
+                if(obj.has("nome") && obj.has("idade")){
+                    String temp_nome = obj.get("nome").getAsString();
+                    int temp_idade = obj.get("idade").getAsInt();
+                    
+                    pessoas[i] = new Pessoa(temp_nome, temp_idade);
+                }
+                System.out.println("" + Arrays.toString(pessoas));
+            }
+            
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }finally{
+            try {
+                reader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+        
+    }
+    
+}
